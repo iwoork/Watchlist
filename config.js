741aa47e19f13f08fe419c50b77407aac3f664f5
Efren Gomez Macasaet <efren@iwoork.com>
2013-03-28 20:25:09 +0800

    Changed config.js

--- config.js
+++ config.js
@@ -2,26 +2,27 @@ exports.config = {
     // Base URL of the App (must be a publically accessible URL)
 
     //What is this?
-    redirect_uri:  '',
+    redirect_uri:  'http://local.chowhere.com',
 
     // Facebook App ID/API Key
-    client_id:     '1111111111111111',
+    client_id:     '308870105905592',
 
     // Facebook Application Secret
-    client_secret: 'zzzzzzzzzzzzzzzzzzzzzzzzzz',
+    client_secret: '10c9a1537adfe073bcf565eeee65cdcb',
 
     // MongoDB endpoint
     //mongoDb:       'mongodb://USER:PASS@SERVER:PORT/DATABASE',
-    mongoDb:       'mongodb://user:mypass@aaaaaaa.mongolab.com:27847/watchlist',
+    //mongoDb:       'mongodb://user:mypass@aaaaaaa.mongolab.com:27847/watchlist',
+    mongoDb: 'mongodb://mongochow:k3f12eNn@ds035237.mongolab.com:35237/chowheredb',
 
     // Session encyption key
     sessionSecret: 'something12345secret',
 
     //What is this?
-    appUrl: 'http://app_id_on_heroku.herokuapp.com',
+    appUrl: 'local.chowhere.com',
 
     //What is this?
-    fbNamespace: 'facebookappnamespace',
+    fbNamespace: 'chowhere',
 
     rottenTomatoesApiKey: '12341243531425642564356'
 };
