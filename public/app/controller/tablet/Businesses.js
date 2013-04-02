c7c2641ede59a9490d35755027ea0be264496070
Efren Gomez Macasaet <efren@iwoork.com>
2013-04-02 22:05:04 +0800

    Push

---
+++ public/app/controller/tablet/Businesses.js
@@ -0,0 +1,48 @@
+/**
+ * The tablet specific controller for Movies
+ */
+Ext.define('WL.controller.tablet.Businesses', {
+
+    extend: 'WL.controller.Businesses',
+
+    config: {
+        refs: {
+            tabletContainer: 'tabletContainer'
+        }
+    },
+
+    init: function() {
+
+        this.callParent();
+
+        WL.Facebook.on({
+            connected: this.onFacebookLogin,
+            logout: this.onFacebookLogout,
+            unauthorized: this.onFacebookUnauthorized,
+            scope: this
+        });
+    },
+
+    onFacebookLogin: function() {
+        this.callParent(arguments);
+        this.initContainer();
+    },
+
+    initContainer: function() {
+        if (!this.mainContainer) {
+            this.mainContainer = Ext.Viewport.add({ xtype: 'tabletContainer' });
+        }
+    },
+
+    showMovie: function(record) {
+        WL.currentMovie = record;
+
+        if (!this.businessDetailCmp) {
+            this.businessDetailCmp = Ext.widget('businessDetail');
+        }
+
+        this.businessDetailCmp.setRecord(record);
+        this.getToolbar().setTitle(record.get('title'));
+        this.getTabletContainer().setActiveItem(this.businessDetailCmp);
+    }
+});
