70bf2e218ef4f8e47e48254035a89e4d73dacea0
Efren Gomez Macasaet <efren@iwoork.com>
2013-04-04 00:18:41 +0800

    Push

--- public/app/store/Businesses.js
+++ public/app/store/Businesses.js
@@ -3,16 +3,32 @@ Ext.define('WL.store.Businesses', {
     extend  : 'Ext.data.Store',
 
     config: {
+    	storeId: 'Businesses',
         model: 'WL.model.Business',
-        encodeRequest: true,
-        method: 'GET',
-        limitParam: false,
-        enablePagingParams: false,
-        startParam: false,
-        type: 'rest',
         autoLoad: true,
-        reader: {
+        proxy: {
+          url: 'https://api.mongolab.com/api/1/databases/chowheredb/collections/restaurants',
+          type: 'rest',
+          encodeRequest: true,
+          method: 'GET',
+          limitParam: false,
+          enablePagingParams: false,
+          startParam: false,
+          reader: {
+          },
+          extraParams: {
+            view: 'json',
+            l: 10,
+            s: Ext.encode({'rating':{'positive': -1}}),
+            apiKey: '5083ab6ae4b0940f2c2e5db7',
+            q: Ext.encode({
+              'coordinates':{
+                '$maxDistance': 30
+              }
+            })
+          }
         }
+      }
 
         
 //
@@ -25,5 +41,5 @@ Ext.define('WL.store.Businesses', {
 //                rootProperty: 'businesses'
 //            }
 //        }
-    }
+
 });
